a1263d55305b86b548530dfac4655dde5876d29f
Nour Al Salhi <nuraym9998@gmail.com>
2023-01-06 14:41:16 +0200

    create item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import CartCounter from './pages/cartCounter';
+//style
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <CartCounter />
     </div>
   );
 }
--- src/pages/cartCounter/CartCounter.jsx
+++ src/pages/cartCounter/CartCounter.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, { useCallback, useState } from 'react'
+//style
+import './CartCounter.css'
 const CartCounter = () => {
-  return (
-    <div>CartCounter</div>
-  )
+    //state
+    const [count, setCount] = useState(0)
+    //functions
+    const incerement = useCallback(() => setCount(count + 1), [count]) // TODO: why useCallback? ,how we can enhancing this code without useCallback?
+    const decerement = useCallback(() => {
+        if (count > 0)
+            setCount(count - 1)
+    }, [count])
+    return (
+        <div>
+            <h1>CartCounter <span style={{ color: 'red' }}>{count}</span></h1>
+            <div className='item-counter'>
+                <p>{count}</p>
+                <div>
+                    <button onClick={incerement}>+</button>
+                    <button onClick={decerement}>-</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default CartCounter
